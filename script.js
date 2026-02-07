acb4cc4d50fd9394dd85d70c7fc4a719a313b13f
quyenphamssa10-web <quyenphamssa10@gmail.com>
2026-02-07 09:49:08 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+// Your JavaScript code here
+/*cau truc tuong tac app notes
+1. nut menu(mo tab)
+2. dieu huong trang app
+ - nut trang note(trang chu)
+ - nut trang nhiem vu (trang nhiem vu)
+3. nut tao them
+ - nut tao note (mo trang hien thi chinh sua note moi)
+4. 
+*/
+/* cau truc phan hoi cua app
+1. thanh tab (keo ra phai chiem 70% - nhan tuong tac - hien popup - dong tab tu phai trai)
+2. giao dien chinh sua note(hien trang chinh sua note - luu dinh ky - luu trang - tat trang hien thi)
+3. trang nhiem vu (chuyen trang nhiem vu - dao lai neu bam nguoc lai
+*/
+/* cau truc thuc te cua app
+1. xu ly tin hieu tuong tac(chi nhan-dieu phoi tin hieu cho xu ly)
+- xu ly tin hieu luu tru - cap nhat du lieu tai nguyen
+- xu ly tin hieu tai tai nguyen hien thi
+- xu ly tin hieu 
+2. xu tin hieu yeu cau(logic cua app)
+- xu ly chinh sua u lieu
+- xu ly tai du lieu hien thi
+- xu ly luu & cap nhat du lieu
+3. luu tru du lieu
+- kho du lieu note
+*/
+//nhan va dieu phoi tin hieu
+//xu ly nut tao trang note
+//1.Nhan trang tin hieu bam
+function nhanTinHieuBamTaoNote() {
+  let taoNote = document.getElementById('createNote.html');
+  
+}
+/*tu duy thiet ke trang note
+1.
+*/
+//2.xu
